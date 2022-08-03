1afecccbffe6e3b4c5f6c056bb299d66fe485c08
Nick De Villiers <noodles524@gmail.com>
2022-08-03 16:37:33 +0100

    Make secret key private in an env var

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # FastF1_WebViewer
 A web interface for accessing the functions of the FastF1 Python library.
+
+To generate a secret key:
+
+`python -c 'from django.core.management.utils import get_random_secret_key; print(get_random_secret_key())'`
+
+Copy `.example.env` and paste as `.env`, then add your secret key to the `SECRET_KEY` field.
--- server/settings.py
+++ server/settings.py
@@ -11,6 +11,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+from environ import Env
+
+env = Env()
+
+env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#^v(p9qjp3hu)0z%wc#ghbv+vzu!l1=m5avs6=%fgskhi!qx5w'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True

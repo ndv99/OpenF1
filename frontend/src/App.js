85347ccce79aed53fec1a364a9a916173586537c
Nick De Villiers <noodles524@gmail.com>
2022-08-03 14:17:13 +0100

    Change text and title of landing page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          FastF1 Web Viewer
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/ndv99/FastF1_WebViewer"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Source
         </a>
       </header>
     </div>

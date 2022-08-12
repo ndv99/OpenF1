2d4318f1c74af3ab4ce591cbbace84421fc52a45
Nick De Villiers <nick.devilliers@canonical.com>
2022-08-12 13:58:20 +0100

    Fix env read and implement automatic env creation script

--- manage.py
+++ manage.py
@@ -2,6 +2,7 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from venv import create
 
 
 def main():
@@ -15,6 +16,10 @@ def main():
             "available on your PYTHONPATH environment variable? Did you "
             "forget to activate a virtual environment?"
         ) from exc
+    env_exists = os.path.exists(".env")
+    if not env_exists:
+        import createEnv
+        createEnv.create_env()
     execute_from_command_line(sys.argv)
 
 

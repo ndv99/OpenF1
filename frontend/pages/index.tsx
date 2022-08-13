49e0f9cbb6438a8a5442d4f2bc1b0a04eaed2755
alivesurfin <alivesurfin@gmail.com>
2022-08-13 18:09:01 +0100

    change a tag to next/link tag

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,7 +1,9 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
+
 
 const Home: NextPage = () => {
   return (
@@ -20,7 +22,7 @@ const Home: NextPage = () => {
         <p className={styles.description}>
           Check our progress <a href='https://github.com/users/ndv99/projects/1'>here</a><br />
           View the source code <a href='https://github.com/ndv99/FastF1_WebViewer'>here</a> <br/>
-          View the dev page <a href='/DevPage'>here</a>
+          View the dev page <Link href="/DevPage"><a>here</a></Link>
         </p>
       </main>
 

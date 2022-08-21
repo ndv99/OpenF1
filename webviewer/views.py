9a5152aa78d15f6d572d761b3768b9596bdd4e65
Nick De Villiers <nick.devilliers@canonical.com>
2022-08-21 12:14:39 +0100

    Move response checker for year to own function

--- webviewer/views.py
+++ webviewer/views.py
@@ -14,6 +14,28 @@ def assemble_url(params, url):
             url = f"{url}{p}={params[p]}"
     return url
 
+def get_year_from_request(headers):
+    error = None
+    year = None
+    try:
+        year = int(headers['year'])
+    except ValueError:
+        res = {'errorMessage': "Please send a valid integer for the year you want."}
+        error =  Response(res, status.HTTP_400_BAD_REQUEST)
+    except KeyError:
+        res = {'errorMessage': "Please provide the desired year as a header with key 'year'."}
+        error =  Response(res, status.HTTP_400_BAD_REQUEST)
+
+    if year < 2018:
+        res = {'errorMessage': "Telemetry is only available from 2018 onwards."}
+        error =  Response(res, status.HTTP_400_BAD_REQUEST)
+    
+    if year > 2022:
+        res = {'errorMessage': "It's still 2022, we can't predict the future!"}
+        error =  Response(res, status.HTTP_400_BAD_REQUEST)
+    
+    return error, year
+
 # Create your views here.
 
 class Events(viewsets.ViewSet):
@@ -24,22 +46,11 @@ class Events(viewsets.ViewSet):
         if not os.path.isdir('server/fastf1_cache'):
             os.mkdir('server/fastf1_cache')
         fastf1.Cache.enable_cache('server/fastf1_cache')
-        try:
-            year = int(headers['year'])
-        except ValueError:
-            res = {'errorMessage': "Please send a valid integer for the year you want."}
-            return Response(res, status.HTTP_400_BAD_REQUEST)
-        except KeyError:
-            res = {'errorMessage': "Please provide the desired year as a header with key 'year'."}
-            return Response(res, status.HTTP_400_BAD_REQUEST)
 
-        if year < 2018:
-            res = {'errorMessage': "Telemetry is only available from 2018 onwards."}
-            return Response(res, status.HTTP_400_BAD_REQUEST)
-        
-        if year > 2022:
-            res = {'errorMessage': "It's still 2022, we can't predict the future!"}
-            return Response(res, status.HTTP_400_BAD_REQUEST)
+        error, year = get_year_from_request(headers)
+
+        if error:
+            return error
         
         schedule = fastf1.get_event_schedule(year)
         event_names = schedule.EventName.values.tolist()

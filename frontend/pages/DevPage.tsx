b19f8fd891d3617b91076e90a3258da4ebc39659
Nick De Villiers <nick.devilliers@canonical.com>
2022-09-26 17:13:11 +0100

    Fix code formatting and linting in all TSX files

--- frontend/pages/DevPage.tsx
+++ frontend/pages/DevPage.tsx
@@ -1,52 +1,50 @@
-import YearSelector from './components/YearSelector'
-import type { NextPage } from 'next'
-import { useEffect, useState } from 'react';
+import YearSelector from "./components/YearSelector";
+import type { NextPage } from "next";
+import { useCallback, useEffect, useState } from "react";
 
 const DevPage: NextPage = () => {
-    const [year, setYear] = useState(0)
-    const [events, setEvents] = useState([])
-    const axios = require('axios').default;
+    const [year, setYear] = useState(0);
+    const [events, setEvents] = useState([]);
+    const axios = require("axios").default;
 
-    const getEventsByYear = () => {
-        axios.get("https://fastf1-webviewer-api.herokuapp.com/api/events/", //TODO: add proxy instead of full api url
-            {
-                headers: {
-                    year: year
-                },
-
-            })
+    const getEventsByYear = useCallback(() => {
+        axios
+            .get(
+                "https://fastf1-webviewer-api.herokuapp.com/api/events/", //TODO: add proxy instead of full api url
+                {
+                    headers: {
+                        year: year,
+                    },
+                }
+            )
             .then(function (response: any) {
-                setEvents(response.data)
+                setEvents(response.data);
             })
             .catch(function (error: any) {
-                setEvents([])
-                console.error("Unable to get events for: ", year, error)
-            })
-    }
+                setEvents([]);
+                console.error("Unable to get events for: ", year, error);
+            });
+    }, [axios, year]);
     useEffect(() => {
-        if(year === 0 ){
-            return
-        }        
-        getEventsByYear()
-    },
-        [year]
-    )
+        if (year === 0) {
+            return;
+        }
+        getEventsByYear();
+    }, [year, getEventsByYear]);
 
     useEffect(() => {
-        if(year === 0 ){ //TODO: this will be used when setting year via url query
-            return
+        if (year === 0) {
+            //TODO: this will be used when setting year via url query
+            return;
         }
         //TODO: setevents
-
-    }, [events])
+    }, [events, year]);
 
     return (
         <div>
             <YearSelector setYear={setYear}></YearSelector>
         </div>
+    );
+};
 
-
-    )
-}
-
-export default DevPage
+export default DevPage;

a4a54c55f03e9d8a3ff685aa478b9201cc75a5de
Nick De Villiers <nick.devilliers@canonical.com>
2022-09-29 10:34:09 +0100

    Update YearSelector to use API call to get available years

--- frontend/pages/components/YearSelector.tsx
+++ frontend/pages/components/YearSelector.tsx
@@ -1,33 +1,47 @@
 
 import { Select } from "@canonical/react-components";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 
 const YearSelector = (props: any) => {
     const setYear = props.setYear;
+    const [years, setYears] = useState([{value:0, label: "Select a year", disabled: true}]);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        if (loading) {
+            axios.get("/api/years/")
+            .then((res) => {
+                const tempYears = years;
+
+                for ( var year in res.data["years"] ) {
+                    tempYears.push({ value: parseInt(res.data["years"][year]), label: res.data["years"][year] , disabled:false });
+                };
+
+                setYears(tempYears);
+                setLoading(false);
+            })
+            .catch((err) => {
+                console.error("Unable to get available years. Error: ",  err);
+            });
+        }
+    }, [years, loading]);
     
     const handleChange =(event: any)=>{
         setYear(event.target[event.target.selectedIndex].value);
     };
 
-    const createYearList2018toCurrentYear = () => {
-
-        const list = [{value:0, label: "Select a year", disabled: true}];
-
-        let year = parseInt(new Date().getFullYear().toString().substring(-2));
-
-        do {
-            list.push({ value: year, label: year.toString() , disabled:false});
-            year--;
-        } while (year >= 2018);
-
-        return list;
-    };
-
-
     return (
-        <div id="year-selector-wrapper">
-            <Select defaultValue={0} onChange={handleChange} options={createYearList2018toCurrentYear()} />
-        </div>
+        <>
+            {(loading ? (
+                <p>Loading...</p>
+            ) : (
+                <div id="year-selector-wrapper">
+                    <Select defaultValue={0} onChange={handleChange} options={years} />
+                </div>
+            ))}
+        </>
     );
 };
 

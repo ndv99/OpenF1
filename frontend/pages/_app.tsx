f96da6f8b3a500975390b39d7f3fe8c25eada6d0
Nick De Villiers <nick.devilliers@canonical.com>
2022-09-29 11:33:54 +0100

    Add different env files for prod and dev

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -2,7 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import axios from "axios";
 
-axios.defaults.baseURL = "https://openf1-api.herokuapp.com";
+axios.defaults.baseURL = process.env.NEXT_PUBLIC_QUERY_URL;
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;

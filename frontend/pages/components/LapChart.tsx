b5a8815ec93cd449eb5d47a7613e100950d1b1b6
alivesurfin <alivesurfin@gmail.com>
2022-10-08 20:35:44 +0100

    linting fixes

--- frontend/pages/components/LapChart.tsx
+++ frontend/pages/components/LapChart.tsx
@@ -1,4 +1,4 @@
-import type { NextPage } from 'next'
+import type { NextPage } from "next";
 import {
     Chart as ChartJS,
     CategoryScale,
@@ -8,9 +8,9 @@ import {
     Title,
     Tooltip,
     Legend,
-} from 'chart.js';
+} from "chart.js";
 
-import isEqual from 'lodash.isequal';
+import isEqual from "lodash.isequal";
 
 ChartJS.register(
     CategoryScale,
@@ -21,8 +21,8 @@ ChartJS.register(
     Tooltip,
     Legend
 );
-import { Line } from 'react-chartjs-2'
-import { useRef, useState } from 'react';
+import { Line } from "react-chartjs-2";
+import { useRef, useState } from "react";
 interface DriverLapDataType {
     Name: string;
     Positions: number[];
@@ -39,29 +39,29 @@ interface DriverDataset {
 }
 function createDataObjectFromLapData(lapData: any) {
     console.log(lapData);
-
+    
     if (lapData == undefined || lapData.length < 1) {
-        return false
+        return false;
     }
-    const datasets = []
-    const labels = [] // laps
-    const encounteredTeams: string[] = []
+    const datasets = [];
+    const labels = []; // laps
+    const encounteredTeams: string[] = [];
     //console.log("createDataObj", lapData);
 
-    let maxLaps = [0]
-    const lapDataArray = Object.entries(lapData)
+    let maxLaps = [0];
+    const lapDataArray = Object.entries(lapData);
     lapDataArray.forEach((driverEntry: any) => {
         //console.log(driverEntry);
-        const smallLabel: string = driverEntry[0]
-        const driverLapData: DriverLapDataType = driverEntry[1]
+        const smallLabel: string = driverEntry[0];
+        const driverLapData: DriverLapDataType = driverEntry[1];
         if (driverLapData.Laps.length > maxLaps.length) {
-            maxLaps = driverLapData.Laps
+            maxLaps = driverLapData.Laps;
         }
-        let encounteredTeam = false
+        let encounteredTeam = false;
         if (encounteredTeams.includes(driverLapData.TeamColor)) {
-            encounteredTeam = true
+            encounteredTeam = true;
         } else {
-            encounteredTeams.push(driverLapData.TeamColor)
+            encounteredTeams.push(driverLapData.TeamColor);
         }
         datasets.push({
             label: smallLabel,
@@ -71,61 +71,61 @@ function createDataObjectFromLapData(lapData: any) {
             borderWidth: 1,
             borderDash: encounteredTeam ? [5, 5] : [0, 0],
             pointRadius: 10,
-            pointBorderColor: 'rgba(0, 0, 0, 0)',
-            pointBackgroundColor: 'rgba(0, 0, 0, 0)',
+            pointBorderColor: "rgba(0, 0, 0, 0)",
+            pointBackgroundColor: "rgba(0, 0, 0, 0)",
             pointHoverBorderColor: `#${driverLapData.TeamColor}`,
             pointHoverBackgroundColor: `#${driverLapData.TeamColor}`,
 
-        })
+        });
         // console.log(datasets);
 
-    })
+    });
     let testDataset = {
-        label: 'My First dataset',
-        backgroundColor: 'rgb(255, 99, 132)',
-        borderColor: 'rgb(255, 99, 132)',
+        label: "My First dataset",
+        backgroundColor: "rgb(255, 99, 132)",
+        borderColor: "rgb(255, 99, 132)",
         data: [0, 10, 5, 2, 20, 30, 45],
-    }
+    };
     const data = {
         labels: maxLaps,
         datasets,
     };
 
-    return data
+    return data;
 }
 function compoundStringToColour(compoundString: string) {
     switch (compoundString) {
-        case 'SOFT':
-            return 'red'
-        case 'HARD':
-            return 'white'
-        case 'MEDIUM':
-            return 'yellow'
+        case "SOFT":
+            return "red";
+        case "HARD":
+            return "white";
+        case "MEDIUM":
+            return "yellow";
         default:
-            return 'none'
+            return "none";
     }
 }
 const LapChart: NextPage = (props: any) => { //TODO: endpoint data types
     // TODO: SPINNER
     if (props.lapData == undefined) {
-        return <div>Loading...</div>
+        return <div>Loading...</div>;
     }
     console.log(props.lapData);
 
-    const chart = useRef(null) //create reference hook
+    const chart = useRef(null); //create reference hook
     const [tooltip, setTooltip] = useState({
         opacity: 0,
         top: 0,
         left: 0,
-        lap: '',
-        position: '',
+        lap: "",
+        position: "",
         colour: "#000",
-        border: 'solid',
-        longname: 'tempdrivername',
-        compound: 'tempcompound',
-        laptime: 'templaptime'
-    })
-    const lapData = createDataObjectFromLapData(props.lapData)
+        border: "solid",
+        longname: "tempdrivername",
+        compound: "tempcompound",
+        laptime: "templaptime"
+    });
+    const lapData = createDataObjectFromLapData(props.lapData);
     const title: string = props.name;
     //console.log(title);
     // console.log(tooltip);
@@ -136,28 +136,28 @@ const LapChart: NextPage = (props: any) => { //TODO: endpoint data types
             <Line data={lapData} ref={chart}
                 options={{
 
-                    scales: { y: { position: 'left', reverse: true, ticks: { autoSkip: false, stepSize: 1 } } },
+                    scales: { y: { position: "left", reverse: true, ticks: { autoSkip: false, stepSize: 1 } } },
                     plugins: {
-                        legend: { position: 'right' }, title: { text: title, display: true, color: 'white', fullSize: true },
+                        legend: { position: "right" }, title: { text: title, display: true, color: "white", fullSize: true },
                         tooltip: {
                             enabled: false,
                             external: context => {
-                                const tooltipModel = context.tooltip
-                                if (!chart || !chart.current) return
+                                const tooltipModel = context.tooltip;
+                                if (!chart || !chart.current) return;
 
                                 if (tooltipModel.opacity === 0) {
-                                    if (tooltip.opacity !== 0) setTooltip(prev => ({ ...prev, opacity: 0 }))
-                                    return
+                                    if (tooltip.opacity !== 0) setTooltip(prev => ({ ...prev, opacity: 0 }));
+                                    return;
                                 }
-                                const position = context.chart.canvas.getBoundingClientRect()
+                                const position = context.chart.canvas.getBoundingClientRect();
                                 console.log("test", tooltipModel.dataPoints);
-                                const longname = props.lapData[tooltipModel.dataPoints[0].dataset.label].Name
-                                const compound = props.lapData[tooltipModel.dataPoints[0].dataset.label].Compounds[tooltipModel.dataPoints[0].dataIndex]
-                                const laptime = props.lapData[tooltipModel.dataPoints[0].dataset.label].LapTimes[tooltipModel.dataPoints[0].dataIndex]
+                                const longname = props.lapData[tooltipModel.dataPoints[0].dataset.label].Name;
+                                const compound = props.lapData[tooltipModel.dataPoints[0].dataset.label].Compounds[tooltipModel.dataPoints[0].dataIndex];
+                                const laptime = props.lapData[tooltipModel.dataPoints[0].dataset.label].LapTimes[tooltipModel.dataPoints[0].dataIndex];
 
                                 const index = tooltipModel.dataPoints[0].datasetIndex;
                                 const colour = tooltipModel.dataPoints[0].dataset.borderColor;
-                                const border = tooltipModel.dataPoints[0].dataset.borderDash[0] == 0 ? 'solid' : 'dashed'
+                                const border = tooltipModel.dataPoints[0].dataset.borderDash[0] == 0 ? "solid" : "dashed";
                                 const newTooltipData = {
                                     opacity: 1,
                                     left: position.left + tooltipModel.caretX,
@@ -170,8 +170,8 @@ const LapChart: NextPage = (props: any) => { //TODO: endpoint data types
                                     compound: compound,
                                     laptime: laptime,
 
-                                }
-                                if (!isEqual(tooltip, newTooltipData)) setTooltip(newTooltipData)
+                                };
+                                if (!isEqual(tooltip, newTooltipData)) setTooltip(newTooltipData);
                             },
                         }
                     },
@@ -181,7 +181,7 @@ const LapChart: NextPage = (props: any) => { //TODO: endpoint data types
 
             </Line>
 
-            <div className='tooltip' style={{ backgroundColor: 'rgb(255,255,255,0.5)', padding: "5px", borderRadius: "5px", position: "absolute", top: tooltip.top, left: tooltip.left, opacity: tooltip.opacity, borderRight: `5px ${tooltip.border} ${tooltip.colour}` }}>
+            <div className='tooltip' style={{ backgroundColor: "rgb(255,255,255,0.5)", padding: "5px", borderRadius: "5px", position: "absolute", top: tooltip.top, left: tooltip.left, opacity: tooltip.opacity, borderRight: `5px ${tooltip.border} ${tooltip.colour}` }}>
                 <p>{tooltip.longname}</p>
                 <p>Lap {tooltip.lap} </p>
                 <p>Position {tooltip.position} </p>
@@ -190,8 +190,8 @@ const LapChart: NextPage = (props: any) => { //TODO: endpoint data types
 
             </div>
         </div>
-    )
+    );
 
-}
+};
 
-export default LapChart
+export default LapChart;
